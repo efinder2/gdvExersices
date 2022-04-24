1e9ae8e44677e52223b358b6fe387097a1b7f41c
efinder2 <daniel.brenzel@web.de>
2022-04-24 13:43:44 +0200

    solved 02.1

--- src/02/gradient.ts
+++ src/02/gradient.ts
@@ -12,6 +12,17 @@ import { swapBuffers } from './setup-gradient';
  * @param height The height of the canvas
  */
 export function gradient(data: Uint8ClampedArray, x: number, y: number, width: number, height: number) {
+    let offset: number = y * width + x;
+    let index: number = offset * 4;
+    let multiplier: number = 255 / width;
+
+    let value: number = Math.ceil(multiplier * x);
+    data[index] = value;
+    data[index + 1] = value;
+    data[index + 2] = value;
+    data[index + 3] = 255;
+
+
 
     // TODO: Compute the position of pixel (x, y) in the linearised 'data' array. Each pixel is using 4 bytes in the data array, one each for red, green, blue and alpha.
     // TODO:  Set the red, green and blue components of pixel (x, y) to draw a gradient from black (0) to white (255). Set the alpha component to maximum (255).
