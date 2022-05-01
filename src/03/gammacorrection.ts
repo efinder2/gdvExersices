ee9d669be9572748c9221059678dd1c982a36588
efinder2 <daniel.brenzel@web.de>
2022-05-01 11:39:03 +0200

    solved gamma correction

--- src/03/gammacorrection.ts
+++ src/03/gammacorrection.ts
@@ -15,4 +15,28 @@ export function gammaAdjust(gamma: number, source: Uint8ClampedArray,
                             width: number, height: number) {
 
     // TODO: Perform a gamma correction with the given gamma value on the current pixel at position (x, y) in the source array, and store the result in the dest array.
+        // TODO: Limit the brightness of each color channel to the set of 4 different values 0, 85, 170, 255.
+    // TODO: Set the RGBA values in the target array accordingly.
+    // TODO:
+
+    // TODO: Convert the pixel at position (x, y) in the source array from RGB to XYZ. Limit the 
+    // TODO: Limit the brightness to the set of 4 different values 0, 85, 170, 255.
+    // TODO: Set the RGBA values in the target array to this brightness.
+    let index: number = y * width + x;
+    let positionInArray: number = index * 4;
+
+    let r = source[positionInArray];
+    let g = source[positionInArray + 1];
+    let b = source[positionInArray + 2];
+    let rgb = [r, g, b];
+
+    // grey = floor(I^(1/gamma)*2^n) -> see lecture 02 page 52
+    for (let i = 0; i < 3; i++) {
+        let colorValueInRGB = rgb[i] / 255;
+        let poweer = Math.pow(colorValueInRGB, 1 / gamma);
+        let n = Math.floor(poweer * 255);
+
+        dest[positionInArray + i] = n;
+    }
+    dest[positionInArray + 3] = 255;
 }
